c6dcb37f298c37b73a62d9ee73ea5b4f6640b908
minh-de-rien <77995908+minh-de-rien@users.noreply.github.com>
2024-01-25 23:33:57 -0500

    Linter: trying to deal with the linter error

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 
 export const SITE_TITLE = "Minh de rien";
 export const SITE_DESCRIPTION = "Welcome to my website!";
